e2f3e12a2e48fa4089f2c8a16c0f1da81cf5ba33
Danny <danny.brogno@gmail.com>
2021-03-11 16:48:08 +0100

    Progression

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+// CREATING FUNCTION WITH ID "CALCULATE"
 
+var priceBtn = document.getElementById('calculate');
+priceBtn.addeventListener('click', function(){
+
+  var burgerName = document.getElementById('burger-name').value;
+
+
+});
